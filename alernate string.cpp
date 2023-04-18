1a77363e86a0479c0f580543595c9dbd8596ba32
iam-satyam <116292778+iam-satyam@users.noreply.github.com>
2023-04-18 23:28:17 +0530

    Update alernate string.cpp

--- alernate string.cpp
+++ alernate string.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    string mergeAlternately(string word1, string wrd2) {
+    string mergeAlternately(string word1, string word2) {
         int i = 0;
         int j = 0;
         int M = word1.size();
